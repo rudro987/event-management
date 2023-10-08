7dff51652aee606e875bd73c924d6df1817de525
Tanvir Rudro <rudro987@gmail.com>
2023-10-08 21:33:01 +0600

    firebase project created and firebase integrated in the application

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwOWQlNn4rfeAFhdWaAZgcK9ggChzWboo",
+  authDomain: "event-management-19ddb.firebaseapp.com",
+  projectId: "event-management-19ddb",
+  storageBucket: "event-management-19ddb.appspot.com",
+  messagingSenderId: "922755074414",
+  appId: "1:922755074414:web:15d9a51be69e0e5082197f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
